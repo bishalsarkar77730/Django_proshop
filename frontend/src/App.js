2cae33be0fded08e780a317c542929b1ec671976
Bishal Sarkar <69084883+bishalsarkar77730@users.noreply.github.com>
2022-08-31 23:44:26 +0530

    Home Screen Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 // Import Components starts
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 // Import Components End
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
