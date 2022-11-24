b301564e6c80012701ffa174cf27043ef79dd7f2
Bishal Sarkar <69084883+bishalsarkar77730@users.noreply.github.com>
2022-11-24 10:52:12 +0530

    backend started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,8 +16,8 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route exact path="/" element={<HomeScreen />} />
-            <Route exact path="/product/:id" element={<ProductScreen />} />
+            <Route exact path="/" element={<HomeScreen/>} />
+            <Route exact path="/product/:id" element={<ProductScreen/>} />
           </Routes>
         </Container>
       </main>
